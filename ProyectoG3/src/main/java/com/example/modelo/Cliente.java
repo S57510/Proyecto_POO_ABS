700f206a016674dbb4eab37455ccb90bc6800252
S57510 <150640220+S57510@users.noreply.github.com>
2025-06-29 06:15:18 +0000

    avance de la clase cliente

--- ProyectoG3/src/main/java/com/example/modelo/Cliente.java
+++ ProyectoG3/src/main/java/com/example/modelo/Cliente.java
@@ -2,5 +2,27 @@ package com.example.modelo;
 
 public class Cliente extends Persona{
 
+    private boolean cliente;
+
+    public Cliente(String identificacion, String nombre, String direccion, String telefono, boolean tipoCliente) {
+        super(identificacion, nombre, telefono, direccion);
+        this.cliente = tipoCliente;
+    }
+    
+    public String toString(){
+        return "Cliente{ identificacion "+ getIndentificacion() + 
+               "\nnombre " + getNombre() + 
+               "\ndireccion " + getDireccion() + 
+               "\ntelefono " + getTelefono() + 
+               "\ncliente " + cliente + " }";
+    }
+
+    public void mostrarDatos() {
+        System.out.println(this.toString());
+    }
+
+    public void agregarCliente(String identificacion, String nombre, String direccion, String telefono, boolean tipoCliente){
+        // agregar a la lista de clientes
+    }
     
 }
