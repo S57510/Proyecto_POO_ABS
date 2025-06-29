1b541d11afb9909d048354c8594097e5ff1f777d
S57510 <150640220+S57510@users.noreply.github.com>
2025-06-29 06:25:06 +0000

    avancé la clase proovedor

--- ProyectoG3/src/main/java/com/example/modelo/Proovedor.java
+++ ProyectoG3/src/main/java/com/example/modelo/Proovedor.java
@@ -1,1 +1,25 @@
+package com.example.modelo;
 
+public class Proovedor extends Persona{
+    private String descripcion;
+
+    public Prooverdor(String nombre, String identificacion, String telefono, String descripcion) {
+        super(identificacion, nombre, telefono);
+        this.descripcion = descripcion;
+    }
+
+    public String toString(){
+        return "Proovedor{ identificacion " + getIdentificacion() + 
+               "\nnombre " + getNombre() + 
+               "\ntelefono " + getTelefono() + 
+               "\ndescripcion " + descripcion + " }";
+    }
+
+    public void mostrarDatos(){
+        System.out.println(this.toString());
+    }
+
+    public void agregarProovedor(String identificacion, String nombre, String telefono, String descripcion){
+        // agregar a la lista de proovedores
+    }   
+}
