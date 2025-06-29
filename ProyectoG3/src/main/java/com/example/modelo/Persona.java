aee3da302440df4d6bbad49b4038367433db76f5
ange-j <angevega@espol.edu.ec>
2025-06-28 23:09:31 -0500

    clase Persona avance

--- ProyectoG3/src/main/java/com/example/modelo/Persona.java
+++ ProyectoG3/src/main/java/com/example/modelo/Persona.java
@@ -1,5 +1,20 @@
 package com.example.modelo;
 
 public class Persona {
+    protected String identificacion;
+    protected String nombre;
+    protected String telefono;
+
+    public Persona(String identificacion, String nombre, String telefono){
+        this.identificacion= identificacion;
+        this.nombre= nombre;
+        this.telefono= telefono;
+    }
+    public String toString(){
+        return  "Identificacion: " +this.identificacion+ " Nombre: " + this.nombre + " Telefono: " +this.telefono; 
+    }
+    public String getIdentificacion(){
+        return identificacion;
+    }
     
 }
