4ebcf90f75f9abe555db3455a8e766014a49942a
byronagm <byrguale@espol.edu.ec>
2025-06-29 01:42:47 -0500

    Cambio realizados clases 5,11

--- ProyectoG3/src/main/java/com/example/modelo/OrdenServicio.java
+++ ProyectoG3/src/main/java/com/example/modelo/OrdenServicio.java
@@ -1,1 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class OrdenServicio {
+    private String identificacionCliente;
+    private String fechaServicio;
+    private String tipoVehiculo;
+    private String placaVehiculo;
+    private List<DetalleOrden> detalles;
+    private double total;
+    private Tecnico tecnicoAsignado;
+
+    public OrdenServicio(String identificacionCliente, String fechaServicio, String tipoVehiculo, String placaVehiculo) {
+        this.identificacionCliente = identificacionCliente;
+        this.fechaServicio = fechaServicio;
+        this.tipoVehiculo = tipoVehiculo;
+        this.placaVehiculo = placaVehiculo;
+        this.detalles = new ArrayList<>();
+        this.total = 0;
+    }
+
+    public void agregarServicio()
+}
