3f51dd139df5d8a8e34f1a1aa041f1ac29419947
byronagm <byrguale@espol.edu.ec>
2025-06-29 01:45:30 -0500

    Clase Factura

--- ProyectoG3/src/main/java/com/example/modelo/Factura.java
+++ ProyectoG3/src/main/java/com/example/modelo/Factura.java
@@ -1,1 +1,53 @@
 
+import java.util.ArrayList;
+import java.util.List; 
+public class Factura {
+    private String codigo;
+    private String fechaF;
+    private OrdenServicio ordenServicio;
+    private List<DetalleOrden> detalles;
+
+    public Factura (String codigo, String fecha){
+        this.codigo = codigo;
+        this.fechaF = fechaF;
+        this.ordenServicio = ordenServicio;
+        this.detalles = new ArrayList<>();
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFechaF() {
+        return fechaF;
+    }
+
+    public void setFechaF(String fechaF) {
+        this.fechaF = fechaF;
+    }
+
+    public OrdenServicio getOrdenServicio() {
+        return ordenServicio;
+    }
+
+    public void setOrdenServicio(OrdenServicio ordenServicio) {
+        this.ordenServicio = ordenServicio;
+    }
+
+    @Override
+    public String toString() {
+        return "Factura {codigo: " + codigo + ", fechaF: " + fechaF + "}";
+    }
+}
+
+    public String mostrarFactura (){
+
+    }
+
+
+
+
