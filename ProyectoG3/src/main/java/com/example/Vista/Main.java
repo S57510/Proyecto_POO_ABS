dca63185736c2ba94ca9b55538e7157a1f301dbf
ange-j <angevega@espol.edu.ec>
2025-06-29 21:43:22 -0500

    clase controlador y main planteadas

--- ProyectoG3/src/main/java/com/example/Vista/Main.java
+++ ProyectoG3/src/main/java/com/example/Vista/Main.java
@@ -1,7 +1,62 @@
 package com.example;
 
+import controlador.Controlador;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        int opcion;
+        Controlador controlador = new Controlador();
+        Scanner sc = new Scanner(System.in);
+        
+        do {
+            System.out.println("\n*** MENÚ PRINCIPAL ***");
+            System.out.println("1. Agregar Cliente");
+            System.out.println("2. Agregar Proveedor");
+            System.out.println("3. Agregar Técnico");
+            System.out.println("4. Agregar Servicio");
+            System.out.println("5. Listar Servicios");
+            System.out.println("6. Agregar Orden de Servicio");
+            System.out.println("7. Generar Factura");
+            System.out.println("8. Salir");
+            System.out.print("Seleccione una opción e ingrese el número: ");
+
+            int opcion = sc.nextInt();
+            sc.nextLine(); 
+
+            switch (opcion) {
+                case 1:
+                    controlador.agregarCliente();
+                    break;
+                case 2:
+                    controlador.agregarProveedor();
+                    break;
+                case 3:
+                    controlador.agregarTecnico();
+                    break;
+                case 4:
+                    controlador.agregarServicio();
+                    break;
+                case 5:
+                    controlador.listarServicios();
+                    break;
+                case 6:
+                    controlador.agregarOrden();
+                    break;
+                case 7:
+                    controlador.generarFactura();
+                    break;
+                case 8:
+                    controlador.salir();
+                    break;
+                default:
+                    System.out.println("Opción inválida ");
+            }
+
+        } while (opcion != 8);
+
+
+        sc.close();
     }
 }
--- ProyectoG3/src/main/java/com/example/controlador/Controlador.java
+++ ProyectoG3/src/main/java/com/example/controlador/Controlador.java
@@ -1,5 +1,37 @@
 package com.example.controlador;
 
 public class Controlador {
+
+    public void agregarCliente() {
+        //implementar
+    }
+
+    public void agregarProveedor() {
+        //implementar
+    }
+
+    public void agregarTecnico() {
+        //implementar
+    }
+
+    public void agregarServicio() {
+        //implementar 
+    }
+
+    public void listarServicios() {
+        //implementar  
+    }
+
+    public void agregarOrden() {
+        //implementar  
+    }
+
+    public void generarFactura() {
+        //implementar  
+    }
+
+    public void salir() {
+        //implementar 
+    } 
     
 }
